cb78433532a71738a93ed5f13d637c549fca10c7
bakagamedev <noel.abercrombie@hotmail.com>
2018-11-21 23:49:43 +0000

    Relocate attack definitions to attacks.h

--- arduboy-pokemon-test/battleDefines.h
+++ arduboy-pokemon-test/battleDefines.h
@@ -40,32 +40,10 @@ const PROGMEM FlashString stringBattleAction[] =
 	asFlashString(stringBattleRunny),
 };
 
-enum class ElementType : uint8_t
 const PROGMEM char stringBattleRunYes[] = "Yes";
 const PROGMEM char stringBattleRunNo[] = "No";	
 const PROGMEM FlashString stringBattleRun[] = 
 {
-	Normal,
-	Fire,
-	Water,
-	Grass,
 	asFlashString(stringBattleRunYes), 
 	asFlashString(stringBattleRunNo), 
-};
-
-struct AttackType
-{
-	FlashString label;	//label rather than name because name is a reserved arduino thing
-	ElementType type;
-	int8_t power;
-	constexpr AttackType(FlashString string, ElementType type, int8_t power) : label(string), type(type), power(power) {}
-};
-
-const PROGMEM char attackTest1[] = "punch";
-const PROGMEM char attackTest2[] = "flambe";
-
-const PROGMEM AttackType Attacks[] = 
-{
-	AttackType(asFlashString(attackTest1), ElementType::Normal, 10),
-	AttackType(asFlashString(attackTest2), ElementType::Fire, 15),
 };
