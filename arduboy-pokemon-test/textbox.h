615ca641d7082f1ad4fa6f09912861181c942cb9
bakagamedev <noel.abercrombie@hotmail.com>
2018-11-23 23:05:07 +0000

    Fix text reveal

--- arduboy-pokemon-test/textbox.h
+++ arduboy-pokemon-test/textbox.h
@@ -76,7 +76,7 @@ public:
 
 			for(uint8_t x = 0; x < RowLength; ++x)
 			{
-				if((index > reveal) || (text[index] == '\0'))
+				if(index > reveal)
 					return;
 
 				arduboy.print(text[index]);
