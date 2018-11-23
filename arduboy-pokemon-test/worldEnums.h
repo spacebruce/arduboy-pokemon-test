339a65d76ad9acf311b21e0df149084313f823a4
Pharap <2933055+Pharap@users.noreply.github.com>
2018-11-23 00:44:02 +0000

    Move Direction to a separate header file

--- arduboy-pokemon-test/worldEnums.h
+++ arduboy-pokemon-test/worldEnums.h
@@ -1,16 +1,10 @@
 #pragma once
 
+#include "direction.h"
+
 const uint8_t MaxNPC = 16;
 
 const int16_t TileWidth = 16;
 const int16_t TileHeight = 16;
 const int16_t ChunkWidth = 512;
 const int16_t ChunkHeight = 512;
-
-enum class Direction : uint8_t
-{
-	North,
-	East,
-	South,
-	West,
-};
