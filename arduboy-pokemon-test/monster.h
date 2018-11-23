73c6ac285d08fcb87d4e10772f099b914ce69830
bakagamedev <noel.abercrombie@hotmail.com>
2018-11-23 01:01:31 +0000

    Define some monster names

--- arduboy-pokemon-test/monster.h
+++ arduboy-pokemon-test/monster.h
@@ -1,9 +1,16 @@
 #pragma once
 #include "utilities/stat.h"
 
+#include "stringscommon.h"
+
+const PROGMEM char StringSpeciesNone[] = "missingno";
+const PROGMEM char StringSpeciesBike[] = "Aye-aye";
+const PROGMEM char StringSpeciesDumOctopus[] = "Octoface";
 enum class MonsterSpecies : uint8_t
 {
 	None,	
+	AyeAye,
+	Octoface,
 };
 
 class Monster
