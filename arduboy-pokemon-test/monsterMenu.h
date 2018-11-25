2582f59269dfb9068ca7bf8f621371f7a8b8006e
bakagamedev <noel.abercrombie@hotmail.com>
2018-11-25 02:49:43 +0000

    Add draw function to monsterMenu

--- arduboy-pokemon-test/monsterMenu.h
+++ arduboy-pokemon-test/monsterMenu.h
@@ -1,11 +1,20 @@
 #pragma once
+#include <Arduboy2.h>
 
 #include "contextStats.h"
+#include "monster.h"
+#include "menu.h"
 
 class MonsterMenu
 {
 private:
 	ContextStats &stats;
+	
 public:
 	MonsterMenu(ContextStats & stats) : stats(stats) {};
+	
+	void Draw(Arduboy2 & arduboy)
+	{
+		
+	}
 };
