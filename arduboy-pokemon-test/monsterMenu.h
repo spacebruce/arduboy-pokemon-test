32edb5b3d7273a9c346c2595c203a36e99651de7
bakagamedev <noel.abercrombie@hotmail.com>
2018-11-26 01:23:23 +0000

    Add monsterMenu ui placeholder

--- arduboy-pokemon-test/monsterMenu.h
+++ arduboy-pokemon-test/monsterMenu.h
@@ -10,11 +10,26 @@ class MonsterMenu
 private:
 	ContextStats &stats;
 	
+	const uint8_t x = 4;
+	const uint8_t y = 2;
+	const uint8_t width = 120;
+	const uint8_t height = 60;
+	
 public:
 	MonsterMenu(ContextStats & stats) : stats(stats) {};
 	
-	void Draw(Arduboy2 & arduboy)
+	bool active = false;
+	
+	void update(Arduboy2 & arduboy)
 	{
-		
+		if(arduboy.justPressed(A_BUTTON))
+		{
+			this->active = false;
+		}
+	}
+	
+	void draw(Arduboy2 & arduboy)
+	{
+		arduboy.drawRect(x,y,width,height,BLACK);
 	}
 };
