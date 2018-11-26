828ec45aecb9ff2bf16b9208d29e20ef47e487ca
bakagamedev <noel.abercrombie@hotmail.com>
2018-11-26 13:20:04 +0000

    Draw monstermenu in segments

--- arduboy-pokemon-test/monsterMenu.h
+++ arduboy-pokemon-test/monsterMenu.h
@@ -11,12 +11,24 @@ private:
 	Arduboy2 & arduboy;
 	ContextStats &stats;
 	
-	const uint8_t x = 4;
-	const uint8_t y = 2;
-	const uint8_t width = 120;
-	const uint8_t height = 60;
+	
+	
+	const uint8_t menuX = 4;
+	const uint8_t menuY = 0;
+	const uint8_t menuWidth = 120;
+	const uint8_t menuHeight = 64;
+	const uint8_t heightSegment = 16;
+	
+	void drawSegment(uint8_t index, uint8_t y)
+	{
+		auto monster = &stats.Party[index];
+		arduboy.setCursor(menuX + 2,y);
+		
+		arduboy.print(monster->getSpeciesName());
+	}
 	
 public:
+
 	MonsterMenu(Arduboy2 & arduboy, ContextStats & stats) : arduboy(arduboy), stats(stats) {};
 	
 	bool active = false;
@@ -31,21 +43,15 @@ public:
 	
 	void draw()
 	{
-		arduboy.fillRect(x,y,width,height,BLACK);
-		arduboy.drawRect(x,y,width,height,WHITE);
+		arduboy.fillRect(menuX, menuY, menuWidth, menuHeight, BLACK);
+		arduboy.drawRect(menuX, menuY, menuWidth, menuHeight, WHITE);
 		
 		uint8_t count = stats.Party.getCapacity();
-		arduboy.setCursor(0,0);
-		arduboy.print(count);
-		
-		uint8_t drawY = y;
+		uint8_t drawY = menuY;
 		for(uint8_t i = 0; i < count; ++i)
 		{
-			auto monster = &stats.Party[i];
-			arduboy.setCursor(x,drawY);
-			
-			arduboy.print(monster->getSpeciesName());
-			drawY += 8;
+			drawSegment(i, drawY);
+			drawY += heightSegment;
 		}
 	}
 };
