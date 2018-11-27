ddf08f799a910999c4b76459a1e4c29fcfd9cc6c
bakagamedev <noel.abercrombie@hotmail.com>
2018-11-27 23:53:03 +0000

    Add Empty function to Party

--- arduboy-pokemon-test/party.h
+++ arduboy-pokemon-test/party.h
@@ -5,6 +5,19 @@
 class Party
 {
 public:
+	bool Empty()
+	{
+		return (Count == 0);
+		/*
+		for(uint8_t i = 0; i < Capacity; ++i)
+		{
+			if(monsterEmpty(i))
+				return false;
+		}
+		return true;
+		*/
+	}
+	
 	uint8_t Capacity;
 	uint8_t Count;
 	Monster store[6];	
