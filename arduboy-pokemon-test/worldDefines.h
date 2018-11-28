eec7ec98c2112a6e130515d541dac6a029ead8ef
Pharap <2933055+Pharap@users.noreply.github.com>
2018-11-28 13:14:34 +0000

    Change const to constexpr

--- arduboy-pokemon-test/worldDefines.h
+++ arduboy-pokemon-test/worldDefines.h
@@ -1,8 +1,8 @@
 #pragma once
 
-const uint8_t MaxNPC = 16;
+constexpr uint8_t MaxNPC = 16;
 
-const int16_t TileWidth = 16;
-const int16_t TileHeight = 16;
-const int16_t ChunkWidth = 512;
-const int16_t ChunkHeight = 512;
+constexpr int16_t TileWidth = 16;
+constexpr int16_t TileHeight = 16;
+constexpr int16_t ChunkWidth = 512;
+constexpr int16_t ChunkHeight = 512;
