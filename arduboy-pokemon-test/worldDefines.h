94c9549a1f326ea07c863844bc87ddfa3b2f7dad
Pharap <2933055+Pharap@users.noreply.github.com>
2018-11-28 13:16:45 +0000

    Use more appropriate types for constants

--- arduboy-pokemon-test/worldDefines.h
+++ arduboy-pokemon-test/worldDefines.h
@@ -1,8 +1,10 @@
 #pragma once
 
-constexpr uint8_t MaxNPC = 16;
+#include <stddef.h>
 
-constexpr int16_t TileWidth = 16;
-constexpr int16_t TileHeight = 16;
-constexpr int16_t ChunkWidth = 512;
-constexpr int16_t ChunkHeight = 512;
+constexpr size_t MaxNPC = 16;
+
+constexpr size_t TileWidth = 16;
+constexpr size_t TileHeight = 16;
+constexpr size_t ChunkWidth = 512;
+constexpr size_t ChunkHeight = 512;
